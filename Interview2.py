f2b8a1908f3a7331ccfe22ef9a7b3992a00eb6f4
sophiapentakalos <pentakalos.sophia@gmail.com>
2019-07-17 22:43:46 -0400

    Complete.

--- Interview2.py
+++ Interview2.py
@@ -3,4 +3,8 @@ Sophia Pentakalos
 John Pentakalos
 
 JOHN WUZ HERE XD
+
+Check Permutation: Given two strings, write a method a method to decide if one is a permutation of the other.
 '''
+
+set(str1) == set(str2)
