8cd816ceece3b1d4e7e9b30f0004de4947b8f33d
sophiapentakalos <pentakalos.sophia@gmail.com>
2019-07-17 22:49:25 -0400

    Trying

--- Interview2.py
+++ Interview2.py
@@ -3,12 +3,9 @@ Sophia Pentakalos
 John Pentakalos
 
 JOHN WUZ HERE XD
-'''
-#%%
-'''
-Check Permutation: Given two strings, write a method a method to decide if one
-is a permutation of the other.
-'''
-def is_permute(str1, str2):
-    return set(str1) == set(str2)
 
+Check Permutation: Given two strings, write a method a method to decide if one is a permutation of the other.
+'''
+
+def is_Permutation(str1, str2):
+    return sorted(str1) == sorted(str2)
