706ab82aa89053b9aa4f068017773e1bf267808f
John Pentakalos <jxp9pd@virginia.edu>
2019-07-17 22:46:12 -0400

    Changed to method

--- Interview2.py
+++ Interview2.py
@@ -3,8 +3,12 @@ Sophia Pentakalos
 John Pentakalos
 
 JOHN WUZ HERE XD
-
-Check Permutation: Given two strings, write a method a method to decide if one is a permutation of the other.
 '''
+#%%
+'''
+Check Permutation: Given two strings, write a method a method to decide if one
+is a permutation of the other.
+'''
+def is_permute(str1, str2):
+    return set(str1) == set(str2)
 
-set(str1) == set(str2)
