f7036c7b11ed9d08ff82e307bfca092e26d27586
John Pentakalos <jxp9pd@virginia.edu>
2019-07-17 23:15:22 -0400

    cleaned stuff up a bit

--- Interview2.py
+++ Interview2.py
@@ -2,24 +2,28 @@
 Sophia Pentakalos
 John Pentakalos
 
-Check Permutation: Given two strings, write a method a method to decide if one is a permutation of the other.
+Check Permutation: Given two strings, write a method to decide if one is
+a permutation of the other.
 '''
 
-def is_Permutation(str1, str2):
+def is_permutation(str1, str2):
+    '''Sophia sorting permutation solution'''
     return sorted(str1) == sorted(str2)
 
 def is_permute(str1, str2):
-    if len(str1) != len(str2): return False
+    '''John dictionary solution'''
+    if len(str1) != len(str2):
+        return False
     my_dict = {}
     for char in str1:
-        if char not in my_dict: my_dict[char] = 1
-        else: my_dict[char] += 1
-    print(my_dict)
+        if char not in my_dict:
+            my_dict[char] = 1
+        else:
+            my_dict[char] += 1
     for char in str2:
-        if char not in my_dict: return False
-        if my_dict[char] == 0: return False
+        if char not in my_dict or my_dict[char] == 0:
+            return False
         my_dict[char] -= 1
     return True
-    
 #%%
 is_permute('abab', 'baba')
