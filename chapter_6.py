f207a986891d9288f4388ffe00581ec7f0dc020e
John Pentakalos <jxp9pd@virginia.edu>
2019-12-08 18:01:41 -0800

    Added feature selection functions

--- chapter_6.py
+++ chapter_6.py
@@ -1,65 +1,101 @@
 '''Linear Model Selection and Regularization'''
 import itertools
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tnrange, tqdm_notebook
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+pd.set_option('display.max_columns', 20)
 DATA_PATH = '/Users/johnpentakalos/Documents/Research Data/'
 #%%
 def generate_X(n, mu, sigma, cols):
     '''Generates a predictor matrix with the given parameters. Cols refers to
     the number of polynomial degrees generated.'''
     x = np.random.normal(mu, sigma, n)
+#    pdb.set_trace()
     predictors = []
-    #pdb.set_trace()
-    for i in range(cols):
+    for i in range(1, cols):
         predictors.append(np.power(x, i))
     df = pd.DataFrame(predictors).T
     return df
 
-def generate_response(n, mu, sigma, beta):
+def generate_response(n, mu, sigma, beta, d):
     '''Generates random data points n, from a normal distribution centered at
     mu with variance sigma.'''
     noise = np.random.normal(0, 1, 100)
-#    beta = np.random.rand(4) * 3.5
-    X = generate_X(n, mu, sigma, len(beta))
-    y = X.dot(beta) + noise
+    X = generate_X(n, mu, sigma, d)
+    features = X.iloc[:,:len(beta)]
+    y = features.dot(beta) + noise
     return X, y
 
 #%%
+def mallow_cp(RSS, var, n, d):
+    '''Calculates Mallows Cp'''
+    return (RSS + 2 * d * var)/n
+
+def BIC(RSS, var, n, d):
+    '''Calculates BIC Criterion'''
+    return (RSS + np.log(n) * d * var)/(n*var)
+
+def adjusted_r2(RSS, y, d):
+    '''Calculates adjusted r^2'''
+    n = len(y)
+    TSS = np.sum((y - np.mean(y))**2)
+    return 1 - (RSS/(n - d - 1))/(TSS/(n-1))
+    
+#%%
 def fit_linear_reg(X,Y):
     '''Fit linear regression model and return RSS and R squared values'''
-    model_k = linear_model.LinearRegression(fit_intercept = False)
+    model_k = linear_model.LinearRegression(fit_intercept = True)
     model_k.fit(X,Y)
     RSS = mean_squared_error(Y,model_k.predict(X)) * len(Y)
     R_squared = model_k.score(X,Y)
-    print(model_k.coef_)
     return RSS, R_squared
 
-#Initialization variables
 def best_subset(X, y):
+    '''Runs a linear model fit for every possible combination of features'''
     RSS_list, R_squared_list, feature_list = [],[],[]
     numb_features = []
     #Looping over k = 1 to k = 11 features in X
     for k in tnrange(1,len(X.columns) + 1, desc = 'Loop...'):
         #Looping over all possible combinations: from 11 choose k
         for combo in itertools.combinations(X.columns,k):
-            tmp_result = fit_linear_reg(X[list(combo)], y)   #Store temp result 
-            RSS_list.append(tmp_result[0])                  #Append lists
+            tmp_result = fit_linear_reg(X[list(combo)], y)   #Store temp result
+            RSS_list.append(tmp_result[0])                   #Append lists
             R_squared_list.append(tmp_result[1])
             feature_list.append(combo)
-            numb_features.append(len(combo))   
+            numb_features.append(len(combo))
+    variance = np.var(y)
     #Store in DataFrame
     df = pd.DataFrame({'numb_features': numb_features,'RSS': RSS_list, \
                        'R_squared':R_squared_list,'features':feature_list})
+    df['BIC'] = df.apply(lambda x: BIC(x.RSS, variance, len(y), len(x.features)), axis=1)
+    df['Cp'] = df.apply(lambda x: mallow_cp(x.RSS, variance, len(y), len(x.features)), axis=1)
+    df['adj_r2'] = df.apply(lambda x: adjusted_r2(x.RSS, y, len(x.features)), axis=1)
     return df
-#%%
-X = generate_X(100, 7, 3.8, 5)
-beta = np.array([12, 15, 2, 0.1])
-x, y = generate_response(100, 7, 3.8, beta)
-RSS, r2 = fit_linear_reg(x, y)
-#%%
 
-
+def forward_selection(X, y):
+    '''Linear model selection via forward selection'''
+    best_models = []
+    curr_model = []
+    available_features = list(X.columns)
+    #Loop over each possible feature count.
+    for i in range(X.shape[1]):
+        best_score = sys.maxint
+        best_feature = -1
+        for j in 
+        
+        
+    
+    
+#%%
+beta = np.array([15, 2, 1])
+X, y = generate_response(100, 7, 3.8, beta, 10)
+RSS, r2 = fit_linear_reg(X, y)
+result_df = best_subset(X, y)
+result_df.sort_values(by=['R_squared'], ascending=False).head()
+result_df.sort_values(by=['BIC']).head()
+best_model = result_df.sort_values(by=['adj_r2'], ascending=False).head(1)
+#%%
